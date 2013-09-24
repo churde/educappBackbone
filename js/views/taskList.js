97cb42dbb3343d4444b5d3fd138e39a9f79ff59e
Xurde <churde@gmail.com>
2013-09-24 12:24:29 +0200

    1

--- js/views/taskList.js
+++ js/views/taskList.js
@@ -11,9 +11,10 @@ var TaskListView = Backbone.View.extend({
         $(this.el).html('<div class="tasks"></div>');
         
         for (var i = 0; i < tasks.length; i++) {
-//            con("añado tarea " + i)
             $('.tasks', this.el).append(new TaskListItemView({model: tasks[i]}).render().el);
         }
+        
+        $(this.el).append('<div class="row" id="buttonGroup"><div class="col-xs-6 col-md-6"><button type="button" class="btn btn-info btn-lg" onclick="app.router.back()"><span class="glyphicon glyphicon-chevron-left"></span> Volver</button></div></div>');
         
         return this;
     }
