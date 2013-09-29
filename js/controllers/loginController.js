df14749c5268806dc5509ca31cab78de42fd3ff9
Xurde <churde@gmail.com>
2013-09-29 20:34:46 +0200

    2

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -8,56 +8,63 @@ app.loginController = {
 //app.router.navigate('/activity');
 //return;
 
-        var isGuest = _args.isGuest;
+        try {
+            var isGuest = _args.isGuest;
 
-        var user = $("#user").val();
-        var password = $("#psw").val();
+            var user = $("#user").val();
+            var password = $("#psw").val();
 
-        var success = function(data) {
+            var success = function(data) {
 
-            alert("en el success de validateLogin")
+                alert("en el success de validateLogin")
 
-            if (data.status) {
-                app.loginController.login(data.user);
+                if (data.status) {
+                    app.loginController.login(data.user);
 
 //                alert("pongo usuario como logeado y en 1 segundo redirijo a activity");
 
-                window.setTimeout(function() {
-                    app.router.navigate('/activity');
-                }, 1000);
+                    window.setTimeout(function() {
+                        app.router.navigate('/activity');
+                    }, 1000);
 
+                }
+                else {
+                    alert("Login incorrecto");
+                    app.dataModel.currentUser.set({isLogged: false});
+                }
+            }
+
+            if (!isGuest) {
+
+                app.server.validateLogin({
+                    data: {
+                        user: user,
+                        password: password
+                    },
+                    success: success
+                });
             }
             else {
-                alert("Login incorrecto");
-                app.dataModel.currentUser.set({isLogged: false});
+                alert("SI ES GUEST")
+                app.loginController.login({
+                    name: 'Invitado',
+                    isGuest: true
+                });
+                app.router.navigate('/activity', true);
             }
+        } catch (e) {
+            alert(e)
         }
 
-        if (!isGuest) {
-            
-            app.server.validateLogin({
-                data: {
-                    user: user,
-                    password: password
-                },
-                success: success
-            });
-        }
-        else {
-            alert("SI ES GUEST")
-            app.loginController.login({
-                name: 'Invitado',
-                isGuest: true
-            });
-            app.router.navigate('/activity', true);
-        }
+
+
 
 
 
 
     },
     login: function(user) {
-alert("en app.login pongo al usuario como logeado")
+        alert("en app.login pongo al usuario como logeado")
         app.dataModel.currentUser.set(app.utils.combineJson(user, {isLogged: true}));
         alert("en app.login despues de poner al usuario como logueado")
     },
