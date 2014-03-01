6472601540237152c45a606705725851a4595ddd
Tali <Tali@Tali-VAIO>
2014-03-01 12:33:55 +0100

    nuevas plantillas del gestor

---
+++ templates-gestor/lib/js/main.js
@@ -0,0 +1,29 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var app;
+
+app = {
+    correccion: {
+        init: function() {
+            app.addTooltip('myId', 'myTitle', 'fdsa');
+        }
+
+    },
+    panel: {
+        init: function() {
+            app.addTooltip('myId', 'myTitle', 'fdsa');
+            app.addTooltip('myId', 'myTitle', 'fdsa');
+            app.addTooltip('myId', 'myTitle', 'fdsa');
+        }
+    },
+    addTooltip: function(id, title, placement) {
+        $('#' + id).tooltip({
+            title: title,
+            placement: placement
+        });
+    }
+
+};
